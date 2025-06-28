02a1f495bed232ca905a263f698958998f6f3dea
helaouichourouk <chourouk.helaoui2@gmail.com>
2025-06-28 14:33:47 +0100

    update

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -53,10 +53,10 @@ export default function Projects() {
             ))}
           </div>
           <div className="mx-auto mt-16 max-w-5xl text-center text-foreground md:mt-28">
-            <span className="text-xl font-bold md:text-2xl">
+            {/* <span className="text-xl font-bold md:text-2xl">
               I am currently building new projects and learning backend
               development to expand my skill set beyond frontend.
-            </span>
+            </span> */}
             <p className="mt-10 text-base md:text-xl">
               Visit my github to see some of the latest projects{" "}
               <a
